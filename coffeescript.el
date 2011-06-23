d4fdb4f036c611835422668ec298bef84389bf19
Forrest Chang <fchang@hedgeye.com>
2011-06-22 17:22:39 -0700

    * add coffeescript suport

---
+++ coffeescript.el
@@ -0,0 +1,6 @@
+(add-to-list 'load-path (concat user-specific-dir "/coffee-mode"))
+
+(require 'coffee-mode)
+
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
